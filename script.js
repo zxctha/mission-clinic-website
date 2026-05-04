59dbb30280efabc8fb8b88a34cf627bf20360088
zxctha <tubillo.marlon@gmail.com>
2026-05-04 14:08:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -62,13 +62,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-window.addEventListener("scroll", function () {
-  const header = document.querySelector("header");
-
-  if (window.scrollY > 50) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
-});
 
