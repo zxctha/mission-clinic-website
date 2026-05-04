c595e9fb4274bd64ae5f3f1da2b471bab9becf0c
zxctha <tubillo.marlon@gmail.com>
2026-05-04 14:04:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -61,3 +61,14 @@ document.addEventListener('DOMContentLoaded', () => {
         lucide.createIcons();
     }
 });
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
